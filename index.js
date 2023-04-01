6c289fb4fa6110792be414a2c661131fd0d92cc6
Geronimo Cnochaert <gerocnochaert@hotmail.com>
2023-04-01 17:58:14 -0300

    Primera actualizacion para pre_entrega3

--- index.js
+++ index.js
@@ -1,89 +1,92 @@
-// Bienvenida
+const divMotos = document.getElementById('divMotos')
 
-let nombreUsuario = prompt("Ingrese su NOMBRE")
+//clase
 
-while (nombreUsuario === null) {
-    nombreUsuario = prompt("Ingrese su NOMBRE nuevamente")
-    continue
-}
-
-
-// Creacion de class Casa de cambio
-
-class Casa {
-    constructor(nombre, id, dolar, euro, real) {
-        this.nombre = nombre
+class Moto {
+    constructor(id, nombre, precio, image) {
         this.id = id
-        this.dolar = dolar
-        this.euro = euro
-        this.real = real
+        this.nombre = nombre
+        this.precio = precio
+        this.image = image
     }
 }
-
-// Agrego casas de cambio al Array
-
-const casas = [
-    new Casa("Zona Sur", 1, 300, 325, 150),
-    new Casa("Zona Norte", 2, 320, 345, 175),
-    new Casa("Zona Oeste", 3, 315, 330, 165),
-    new Casa("Zona Centro", 4, 390, 400, 200),
-    new Casa("Zona Este", 5, 315, 320, 160)
+const motos = [
+    new Moto(1, 'Yamaha IBR 125', 1300,'img/Yamaha_IBR.jpg'),
+    new Moto(2, 'Bajaj Rouser 125', 1500,'img/Rouser_125.jpg'),
+    new Moto(3, 'Bajaj Dominar 400', 2500,'img/Yamaha_IBR.jpg'),
+    new Moto(4, 'Yamaha FZ 150', 1700,'img/Yamaha_IBR.jpg'),
+    new Moto(5, 'Honda Wave 110', 900,'img/Yamaha_IBR.jpg'),
+    new Moto(6, 'Honda XR 150', 1900,'img/Yamaha_IBR.jpg'),
+    new Moto(7, 'Ninja Kawasaki 400', 5000,'img/Yamaha_IBR.jpg'),
+    new Moto(8, 'Ninja Kawasaki 650', 7000,'img/Yamaha_IBR.jpg'),
+    new Moto(9, 'Kymco Downtown 350', 4000,'img/Yamaha_IBR.jpg'),
+    new Moto(10, 'Honda Twister 250', 3300,'img/Yamaha_IBR.jpg'),
 ]
 
-// eleccion casa de cambio
+motos.forEach(m => {
+    divMotos.innerHTML += `<div class="col-sm-6 mb-3 mb-sm-0 cardMoto">
+    <div class="card">
+    <img src="${m.image}" class="card-img-top">
+      <div class="card-body">
+        <h5 class="card-title">${m.nombre}</h5>
+        <p class="card-text">${m.precio}</p>
+        <button id=${m.id} class="btn btn-primary">AGREGAR</button>
+      </div>`
+})
 
-let casaElegida = parseInt(
-    prompt(
-        'Escoge el numero de Casa de Cambio que deseas cotizar? 1.Zona Sur || 2.Zona Norte || 3.Zona Oeste || 4.Zona Centro || 5.Zona Este'
-    )
-)
+const carrito = []
 
-let eleccionCasa = false
+const agregarBotones = document.querySelectorAll('.btn-primary')
 
-let casa
 
-while (eleccionCasa === false) {
-    casa = casas.find((casa) => casa.id === casaElegida)
-    if (!casa) {
-        casaElegida = parseInt(
-            prompt(
-                'Escoge UN numero de Casa de Cambio que figura entre las opciones: 1.Zona Sur || 2.Zona Norte || 3.Zona Oeste || 4.Zona Centro || 5.Zona Este'
-            )
-        )
-    } else {
-        eleccionCasa = true
+agregarBotones.forEach(btn => {
+    btn.onclick = () => {
+        const moto = motos.find(m => m.id === parseInt(btn.id))
+        const motoCarrito = {
+            id: moto.id,
+            nombre: moto.nombre,
+            precio: moto.precio,
+            cantidad: 1,
+
+        }
+
+        const motoEnCarrito = carrito.find(m=>m.id=== motoCarrito.id)
+        if (!motoEnCarrito) {
+            carrito.push(motoCarrito)
+        } else{
+            motoEnCarrito.cantidad++
+        }
+
+        console.log(carrito)
     }
+})
+
+
+const thead = document.querySelector('#thead')
+const tbody = document.querySelector('#tbody')
+const finalizarBoton = document.querySelector('#finCompra')
+const parrafoPrecioTotal = document.querySelector('#precioTotal')
+
+finalizarBoton.onclick = ()=>{
+    divMotos.remove()
+    finalizarBoton.remove()
+    thead.innerHTML = `<tr>
+    <th scope="col">Moto</th>
+    <th scope="col">Cantidad</th>
+    <th scope="col">Total</th>
+  </tr>
+` 
+let totalCompra = 0
+carrito.forEach(m=>{
+    totalCompra += m.cantidad*m.precio
+    tbody.innerHTML += ` <tr class="table-success">
+    <td>${m.nombre}</td>
+    <td>${m.cantidad}</td>
+    <td>${m.cantidad*m.precio}</td>
+  </tr>`
 }
-
-//funcion de conversion de moneda de cambio
-
-const pesosConvertir = parseInt(
-    prompt("Ingresa los pesos que deseas cambiar")
 )
 
-const convertidor = (n1, n2) => n1 / n2
+parrafoPrecioTotal.innerText = `El total de tu compra es ${totalCompra}`
 
-const dolar = convertidor(pesosConvertir, casa.dolar)
-const euro = convertidor(pesosConvertir, casa.euro)
-const real = convertidor(pesosConvertir, casa.real)
-
-
-alert(
-    `Las opciones que te ofrece ${casa.nombre} son: 1.U$ ${dolar.toFixed(2)} - 2.€$ ${euro.toFixed(2)} - 3.R$ ${real.toFixed(2)}`
-)
-
-
-// Confirmacion de operacion
-
-let pedido = parseInt(prompt("¿Desea confirmar la operación? Elija una de las siguientes opciones con formato numerico: 1 = SI | 2 = NO"))
-
-while (pedido !== 1 && pedido !== 2) {
-    alert("Por favor, vuelva a intentarlo con alguna de las opciones disponibles.")
-    pedido = parseInt(prompt("¿Desea confirmar la operación? Elija una de las siguientes opciones con formato numerico: 1 = SI | 2 = NO"))
-}
-
-if (pedido === 1) {
-    alert(nombreUsuario + " muchas gracias por su compra :)")
-} else {
-    alert(nombreUsuario + " esperamos que pueda realizar una compra en un futuro.")
 }
