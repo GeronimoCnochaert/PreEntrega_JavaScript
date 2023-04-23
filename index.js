2825493c8be04425065a155c42ca7c62b754ed81
Geronimo Cnochaert <gerocnochaert@hotmail.com>
2023-04-22 21:10:47 -0300

    Finalizacion de proyecto Javascript

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ formulario.onsubmit = (e) =>{
     }
     localStorage.setItem('datosUsuario', JSON.stringify(datosUsuario))
     formulario.remove()
-    titulo.innerText = `Bienvenido ${datosUsuario.nombre} ${datosUsuario.apellido} a Tienda tu Moto`
+    titulo.innerText = `Bienvenido ${datosUsuario.nombre} ${datosUsuario.apellido} a Tienda Gero`
 } 
 
 // Storage
@@ -20,77 +20,141 @@ formulario.onsubmit = (e) =>{
 const datosUsuario = JSON.parse(localStorage.getItem('datosUsuario'))
 if(datosUsuario){
     formulario.remove()
-    titulo.innerText = `Bienvenido ${datosUsuario.nombre} ${datosUsuario.apellido} a Tienda tu Moto`
+    titulo.innerText = `Bienvenido ${datosUsuario.nombre} ${datosUsuario.apellido} a Tienda Gero`
 }
 
-//Div motos
-const divMotos = document.getElementById('divMotos')
 
-//clase
+// fetch para todos los productos
+const divProductos = document.getElementById('productos')
+const botonFinalizar = document.getElementById('finalizar')
 
-class Moto {
-    constructor(id, nombre, precio, image) {
-        this.id = id
-        this.nombre = nombre
-        this.precio = precio
-        this.image = image
-    }
+
+const fetchProductos = async () => {
+    const productosApi = await fetch('https://fakestoreapi.com/products')
+    const productosJSON = await productosApi.json()
+    return productosJSON
 }
 
-// Listado de motos
-const motos = [
-    new Moto(1, 'Yamaha IBR 125', 1300,'img/Yamaha_IBR.jpg'),
-    new Moto(2, 'Bajaj Rouser 125', 1500,'img/Rouser_125.jpg'),
-    new Moto(3, 'Bajaj Dominar 400', 2500,'img/bajaj_dominar.jpg'),
-    new Moto(4, 'Yamaha FZ 150', 1700,'img/yamaha_fz_150.jpg'),
-    new Moto(5, 'Honda Wave 110', 900,'img/honda_wave.jpg'),
-    new Moto(6, 'Honda XR 150', 1900,'img/honda_xr_150.jpg'),
-    new Moto(7, 'Ninja Kawasaki 400', 5000,'img/ninja_400.jpg'),
-    new Moto(8, 'Ninja Kawasaki 650', 7000,'img/ninja_650.jpg'),
-    new Moto(9, 'Kymco Downtown 350', 4000,'img/kymco_dowtown_350.jpg'),
-    new Moto(10, 'Honda Twister 250', 3300,'img/honda_twister_250.jpg'),
-]
+// fetch para un los producto
+const fetchUnProducto = async (id) => {
+    const productApi = await fetch(`https://fakestoreapi.com/products/${id}`)
+    const productJSON = await productApi.json()
+    return productJSON
+}
 
-motos.forEach(m => {
-    divMotos.innerHTML += `<div class="col-sm-6 mb-3 mb-sm-0 cardMoto">
-    <div class="card">
-    <img src="${m.image}" class="card-img-top">
-      <div class="card-body">
-        <h5 class="card-title">${m.nombre}</h5>
-        <p class="card-text">${m.precio}</p>
-        <button id=${m.id} class="btn btn-primary">AGREGAR</button>
+
+// funcion para extraer todos los productos
+
+const extraerProductos = async () => {
+    const productos = await fetchProductos()
+    productos.forEach(element => {
+        const { title, id, price, category, image } = element
+        divProductos.innerHTML += `<div class="card cardProducts" style="width: 10rem;">
+        <img src=${image} class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${title}</h5>
+          <p class="card-text">${category}</p>
+          <button type="button" class="btn btn-outline-primary" id=${id} onclick="agregarProducto(${id})">AGREGAR POR $${price}</button>
+          <button type="button" class="btn btn-outline-danger" id=${id} onclick="eliminarProducto(${id})">QUITAR</button>
+        </div>
       </div>`
-})
+    });
+}
 
+extraerProductos()
 // carrito
-const carrito = []
+let carrito = []
 
-// botones
-const agregarBotones = document.querySelectorAll('.btn-primary')
-
-
-agregarBotones.forEach(btn => {
-    btn.onclick = () => {
-        
-        const moto = motos.find(m => m.id === parseInt(btn.id))
-        const motoCarrito = {
-            id: moto.id,
-            nombre: moto.nombre,
-            precio: moto.precio,
+// agregar productos
+const agregarProducto = async (id) => {
+    const product = await fetchUnProducto(id)
+    const buscarProdCarrito = carrito.find(e => e.id === product.id)
+    if (!buscarProdCarrito) {
+        carrito.push({
+            id: product.id,
+            name: product.title,
             cantidad: 1,
-
-        }
-
-        const motoEnCarrito = carrito.find(m=>m.id=== motoCarrito.id)
-        if (!motoEnCarrito) {
-            carrito.push(motoCarrito)
-        } else{
-            motoEnCarrito.cantidad++
-        }
-        
+            price: product.price
+        })
+    } else {
+        buscarProdCarrito.cantidad++
     }
-})
+    msjAgregarProducto()
+    console.log(carrito)
+}
 
+// eliminar productos
+const eliminarProducto = (id) => {
+    const buscarProdCarrito = carrito.find(e => e.id === id)
+    if (!buscarProdCarrito) {
+        msjCeroProducto()
+    }
+    else {
+        if (buscarProdCarrito.cantidad === 1) {
+
+            carrito = carrito.filter((e) => e.id !== id)
+        } else {
+            buscarProdCarrito.cantidad--
+        }
+        msjEliminarProducto()
+    }
+    console.log(carrito)
+}
+
+// mensaje de confirmacion de seleccion de producto  
+const msjAgregarProducto = () => {
+    Swal.fire({
+        text: 'Haz agregado el producto seleccionado',
+        timer: 1500,
+        icon: 'success'
+    })
+}
+
+// mensaje de confirmacion de eliminacion de producto
+const msjEliminarProducto = () => {
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+          confirmButton: 'btn btn-success',
+          cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false
+      })
+      
+      swalWithBootstrapButtons.fire({
+        title: 'Estas seguro de eliminar el producto seleccionado?',
+        text: "No vas a poder recuperarlo!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Si, eliminalo!',
+        cancelButtonText: 'No, quiero mantenerlo!',
+        reverseButtons: true
+      }).then((result) => {
+        if (result.isConfirmed) {
+          swalWithBootstrapButtons.fire(
+            'Eliminado!',
+            'Tu producto ha sido eliminado.',
+            'success'
+          )
+        } else if (
+          result.dismiss === Swal.DismissReason.cancel
+        ) {
+          swalWithBootstrapButtons.fire(
+            'Cancelado',
+            'Tu producto se ha conservado :)',
+            'error'
+          )
+        }
+      })
+}
+
+// mensaje de no hay producto para eliminar
+const msjCeroProducto = () => {
+    Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'No tienes el producto seleccionado en tu carrito!',
+      })
+}
 
 // Listado de productos elegidos / Final de la compra
 const thead = document.querySelector('#thead')
@@ -99,24 +163,24 @@ const finalizarBoton = document.querySelector('#finCompra')
 const parrafoPrecioTotal = document.querySelector('#precioTotal')
 
 finalizarBoton.onclick = ()=>{
-    divMotos.remove()
+    divProductos.remove()
     finalizarBoton.remove()
     thead.innerHTML = `<tr>
-    <th scope="col">Moto</th>
+    <th scope="col">Producto</th>
     <th scope="col">Cantidad</th>
     <th scope="col">Total</th>
   </tr>
 ` 
 let totalCompra = 0
-carrito.forEach(m=>{
-    totalCompra += m.cantidad*m.precio
+carrito.forEach(e=>{
+    totalCompra += e.cantidad*e.price
     tbody.innerHTML += ` <tr class="table-success">
-    <td>${m.nombre}</td>
-    <td>${m.cantidad}</td>
-    <td>${m.cantidad*m.precio}</td>
+    <td>${e.name}</td>
+    <td>${e.cantidad}</td>
+    <td>${e.cantidad*e.price}</td>
   </tr>`
 }
-)
+)   
 
 parrafoPrecioTotal.innerText = `El total de tu compra es ${totalCompra}`
 
